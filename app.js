494c0bf677535738b8da06880c5ddf8a86d802cc
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2022-10-14 19:33:20 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.status(200).json({
+        status: true,
+        message: 'percobaan deploy ke vercel'
+    });
+});
+
+app.use((req, res, next) => {
+    return res.status(404).json({
+        status: false,
+        message: 'are you lost?',
+        data: null
+    });
+});
+
+app.use((err, req, res, next) => {
+    return res.status(500).json({
+        status: false,
+        message: err.message,
+        data: null
+    });
+});
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
